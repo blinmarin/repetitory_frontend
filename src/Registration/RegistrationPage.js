dae14ad27067b7dadb67809f222c95ff955510ff
blinmarin <mbortnikova@yandex.ru>
2023-07-20 21:54:31 +0300

    add registration form

---
+++ src/Registration/RegistrationPage.js
@@ -0,0 +1,20 @@
+import './Registration.css'
+
+function RegistrationPage() {
+    return (
+        <div className='main-wrapper'>
+            <div className="wrapper">
+            <h1 className='headline'>Registration</h1>
+            <div className='log-pas-block'>
+                <p>Login</p>
+                <input></input>
+                <p>Password</p>
+                <input></input>
+            </div>
+        </div>
+        </div>
+        
+    );
+}
+
+export default RegistrationPage
